05d16c4fc55587d0db7df9b4daa30a38cdf998d4
Nik Sheridan <niksheridan@duck.com>
2024-05-21 19:23:36 +0100

    adding assert for empty string

--- pynenv/environment.py
+++ pynenv/environment.py
@@ -8,7 +8,8 @@ def get_environment_variable(env_var):
         logger.success(f'environment variable {env_var} retrrived successfully')
     if not environment_variable:
         logger.error(f'environment variable {env_var} either not set or empty')
-    assert environment_variable != None, f'environment variable {env_var} either not set or empty'
+    assert environment_variable != None, f'environment variable {env_var} not set'
+    assert environment_variable != '', f'environment variable {env_var} empty'
     return environment_variable
 
 
--- tests/test_get_var.py
+++ tests/test_get_var.py
@@ -1,11 +1,19 @@
 import os
+import pytest
 from dotenv import load_dotenv
 from pynenv.environment import get_environment_variable
 
 
 def test_get_environment_variable():
-    test_dir = os.path.dirname(__file__) 
+    test_dir = os.path.dirname(__file__)
     load_dotenv(dotenv_path=f'{test_dir}/.env.mock')
     want = "mock home"
     got = get_environment_variable('MOCK_HOME')
     assert want == got
+
+
+def test_empty_environment_variable():
+    test_dir = os.path.dirname(__file__)
+    load_dotenv(dotenv_path=f'{test_dir}/.env.mock')
+    with pytest.raises(AssertionError):
+        got = get_environment_variable('MOCK_MISSING')
