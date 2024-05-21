e6c1e1a646d9dcc8af81e919743429f95030b8f6
Nik Sheridan <niksheridan@duck.com>
2024-05-21 19:44:08 +0100

    rename test

--- tests/test_vars.py
+++ tests/test_vars.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from pynenv.environment import get_environment_variable
 
 
-def test_get_environment_variable():
+def test_fetch_environment_variable():
     test_dir = os.path.dirname(__file__)
     load_dotenv(dotenv_path=f'{test_dir}/.env.mock')
     want = "mock home"
